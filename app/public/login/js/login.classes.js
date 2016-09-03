99cbaab0853a2dda8015ad3610cd83d6f4120bfe
felixacdc <pfmata01@gmail.com>
2016-09-02 22:50:54 -0600

    [modify] validation login good

--- app/public/login/js/login.classes.js
+++ app/public/login/js/login.classes.js
@@ -2,34 +2,34 @@
 
 var msgRequired = "Campo requerido.";
 
-// (function(){
+(function(){
 
-//     self.Verification = function(user, password, token) {
-//         this.user = user;
-//         this.password = password;
-//         this.token = token;
-//         this.url = "/auth/verify/";
-//     }
+    self.Verification = function(user, password, token) {
+        this.user = user;
+        this.password = password;
+        this.token = token;
+        this.url = "/auth/verify/";
+    }
 
-//     self.Verification.prototype = {
-//         verify: function(callback) {
-//             $.ajax({
-//                 url: this.url,
-//                 headers: {'X-CSRF-TOKEN': this.token},
-//                 type: "POST",
-//                 data: {
-//                     user: this.user,
-//                     password: this.password
-//                 },
-//                 success: function(response) {
-//                     if ( response == "ok") {
-//                         callback(null);
-//                     } else {
-//                         return callback(new Error("Correo o contraseña incorrectas"));
-//                     }
-//                 }
-//             });
-//         }
-//     }
+    self.Verification.prototype = {
+        verify: function(callback) {
+            $.ajax({
+                url: this.url,
+                headers: {'X-CSRF-TOKEN': this.token},
+                type: "POST",
+                data: {
+                    user: this.user,
+                    password: this.password
+                },
+                success: function(response) {
+                    if ( response == "ok") {
+                        callback(null);
+                    } else {
+                        return callback(new Error("Correo o contraseña incorrectas"));
+                    }
+                }
+            });
+        }
+    }
 
-// })();
+})();
